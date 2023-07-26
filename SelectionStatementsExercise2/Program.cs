01cdc27445cbc1a8ebdec04cb3c7db1da22b425a
Royroy205 <leroyjr2344@live.com>
2023-07-25 23:10:13 -0500

    Exercise 2 complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
         }
+    
+        
+
+        public static void FavSubject() 
+        {
+
+            Console.Write("Enter your favorite subject: ");
+            string subject = Console.ReadLine();    
+
+            switch(subject.ToLower()) 
+            {
+                case "math":
+                    Console.WriteLine("I hate math");
+                    break;
+                case "science":
+                    Console.WriteLine("I love science");
+                    break;
+                case "english":
+                    Console.WriteLine("i think english is good comprehension");
+                    break;
+                case "history":
+                    Console.WriteLine("i thik history is great");
+                    break;
+                case "phyical edcation":
+                    Console.WriteLine("Good for the body");
+                    break;
+                default:
+                    Console.WriteLine("Didnt think about that one. That's a good one");
+                    break;
+            
+            
+            
+            
+            }
+        
+        
+        
+        
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
+
+
 }
